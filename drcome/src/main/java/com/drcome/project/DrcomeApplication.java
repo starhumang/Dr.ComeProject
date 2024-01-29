1734976134ad1bf81b4b42c6e748e6e201ad39bb
heejaeRA <rhj7369@nate.com>
2024-01-29 11:26:12 +0900

    init

---
+++ drcome/src/main/java/com/drcome/project/DrcomeApplication.java
@@ -0,0 +1,13 @@
+package com.drcome.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrcomeApplication.class, args);
+	}
+
+}

82030229428651e8702cc13b4c9ae38e5db3f049
다예 <122209197+EstherC1150@users.noreply.github.com>
2024-01-29 17:50:10 +0900

    240129_JPA조인... 해볼까?

--- drcome/src/main/java/com/drcome/project/medical/web/HospitalController.java
+++ drcome/src/main/java/com/drcome/project/medical/web/HospitalController.java
@@ -1,24 +1,27 @@
 package com.drcome.project.medical.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.drcome.project.medical.domain.Hospital;
 import com.drcome.project.medical.repository.HospitalRepository;
 
-@RestController
+@Controller
 public class HospitalController {
 	@Autowired
 	HospitalRepository repo;
 	
-//	@GetMapping(value = {"/hospitalHome"})
-//	public String home() {
-//		return "/hospital/home";
-//	}
+	@GetMapping(value = {"/hospital", "/hospitalHome"})
+	public String home() {
+		return "/hospital/home";
+	}
 	
-	@GetMapping("/hospitalHome")
-	public Iterable<Hospital> hospital() {
-		return repo.findAll();
+	@GetMapping("/hospital/myProfile")
+	public String hospital(Model model) {
+		Iterable<Hospital> hosList = repo.findAll();
+		model.addAttribute("hospitalList", hosList);
+		return "hospital/myprofie";
 	}
 }

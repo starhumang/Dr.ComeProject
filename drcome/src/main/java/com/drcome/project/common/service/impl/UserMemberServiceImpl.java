543e574d24a3f3f113fcf5867197480408117f11
HeeJae RA <rhj7369@nate.com>
2024-02-01 01:53:34 +0900

    ??

--- drcome/src/main/java/com/drcome/project/common/service/impl/UserMemberServiceImpl.java
+++ drcome/src/main/java/com/drcome/project/common/service/impl/UserMemberServiceImpl.java
@@ -1,7 +1,5 @@
 package com.drcome.project.common.service.impl;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,11 +12,6 @@ import com.drcome.project.common.service.UserDetailVO;
 import com.drcome.project.common.service.UserMemberService;
 import com.drcome.project.common.service.UserMemberVO;
 
-import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.model.Message;
-import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
-import net.nurigo.sdk.message.response.SingleMessageSentResponse;
-import net.nurigo.sdk.message.service.DefaultMessageService;
 
 @Service
 public class UserMemberServiceImpl implements UserMemberService, UserDetailsService {
@@ -34,8 +27,6 @@ public class UserMemberServiceImpl implements UserMemberService, UserDetailsServ
 	 
 	@Value("${cool.sms.from.number}")
 	private String fromNumber;
-	
-	private DefaultMessageService messageService;
 
 	@Override
 	public int insertUserMember(UserMemberVO vo) {
@@ -55,23 +46,5 @@ public class UserMemberServiceImpl implements UserMemberService, UserDetailsServ
 		}
 	    return new UserDetailVO(vo);
 	}
-	
-    @PostConstruct
-    public void init() {
-        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, "https://api.coolsms.co.kr");
-    }
-    
-    // 단일 메시지 발송 예제
-    public SingleMessageSentResponse sendOne(String to, String verificationCode) {
-        Message message = new Message();
-		// 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
-        message.setFrom("발신번호 입력");
-        message.setTo(to);
-        message.setText("[Moyiza] 아래의 인증번호를 입력해주세요\n" + verificationCode);
-
-        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
-        return response;
-    }
-
 
 }

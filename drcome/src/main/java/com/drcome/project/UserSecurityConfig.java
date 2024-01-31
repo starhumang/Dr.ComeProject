4bba7234584dd9d411c82b4add5a337b38d65513
HeeJae RA <rhj7369@nate.com>
2024-02-01 01:34:10 +0900

    ?

--- drcome/src/main/java/com/drcome/project/UserSecurityConfig.java
+++ drcome/src/main/java/com/drcome/project/UserSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -28,6 +29,7 @@ public class UserSecurityConfig {
     }
 
 	@Bean
+	@Primary
 	public BCryptPasswordEncoder userPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
--- drcome/src/main/java/com/drcome/project/common/service/impl/UserMemberServiceImpl.java
+++ drcome/src/main/java/com/drcome/project/common/service/impl/UserMemberServiceImpl.java
@@ -1,6 +1,9 @@
 package com.drcome.project.common.service.impl;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,11 +14,28 @@ import com.drcome.project.common.service.UserDetailVO;
 import com.drcome.project.common.service.UserMemberService;
 import com.drcome.project.common.service.UserMemberVO;
 
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+
 @Service
 public class UserMemberServiceImpl implements UserMemberService, UserDetailsService {
 
 	@Autowired
 	UserMemberMapper mapper;
+	
+	@Value("${coolsms.api.key}")
+	private String apiKey;
+	 
+	@Value("${coolsms.api.secret}")
+	private String apiSecretKey;
+	 
+	@Value("${cool.sms.from.number}")
+	private String fromNumber;
+	
+	private DefaultMessageService messageService;
 
 	@Override
 	public int insertUserMember(UserMemberVO vo) {
@@ -35,5 +55,23 @@ public class UserMemberServiceImpl implements UserMemberService, UserDetailsServ
 		}
 	    return new UserDetailVO(vo);
 	}
+	
+    @PostConstruct
+    public void init() {
+        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, "https://api.coolsms.co.kr");
+    }
+    
+    // 단일 메시지 발송 예제
+    public SingleMessageSentResponse sendOne(String to, String verificationCode) {
+        Message message = new Message();
+		// 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
+        message.setFrom("발신번호 입력");
+        message.setTo(to);
+        message.setText("[Moyiza] 아래의 인증번호를 입력해주세요\n" + verificationCode);
+
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        return response;
+    }
+
 
 }

3628c36a9575f6bd91081eb02ffe10be58a09bda
sunjuu12 <148922947+sunjuu12@users.noreply.github.com>
2024-01-31 15:33:26 +0900

    20240131 구현 9일 남았음

--- drcome/src/main/java/com/drcome/project/medical/repository/HospitalRepository.java
+++ drcome/src/main/java/com/drcome/project/medical/repository/HospitalRepository.java
@@ -2,7 +2,7 @@ package com.drcome.project.medical.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.drcome.project.medical.domain.Hospital;
+import com.drcome.project.admin.domain.Hospital;
 
 public interface HospitalRepository extends CrudRepository<Hospital, String>{
 

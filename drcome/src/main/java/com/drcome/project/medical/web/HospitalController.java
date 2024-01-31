ca3d0c4ab48f843b3e8df8e67360c3f9d92091a4
다예 <122209197+EstherC1150@users.noreply.github.com>
2024-01-31 16:18:45 +0900

    공통모델엑

--- drcome/src/main/java/com/drcome/project/medical/web/HospitalController.java
+++ drcome/src/main/java/com/drcome/project/medical/web/HospitalController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.drcome.project.admin.domain.Hospital;
 import com.drcome.project.medical.service.DoctorVO;
@@ -18,25 +18,24 @@ public class HospitalController {
 //	HospitalRepository repo;
 	HospitalService hospitalService;
 	
+	//공통 병원 정보 따로 빼기
+	@ModelAttribute("hospitalSel")
+	public Hospital getServerTime() {
+		String hospitalId = "krrlo";
+	    Hospital hosSel = hospitalService.findByhospitalId(hospitalId);
+	    return hosSel;
+	}	
+	
+	//병원 마이페이지 메인-대시보드
 	@GetMapping(value = {"/hospital", "/hospitalHome"})
 	public String home() {
 		return "/hospital/home";
 	}
-	
-//	@GetMapping("/hospital/myProfile")
-//	public String hospital(Model model) {
-//		Iterable<Hospital> hosList = repo.findAll();
-//		model.addAttribute("hospitalList", hosList);
-//		return "hospital/myprofile";
-//	}
-	
-	//병원 프로필
+
+	//병원 마이프로필
 	//병원 단건조회(id로) + 병원-의사 조회
-	@GetMapping("/hospital/myProfile/{hospitalId}")
-	public String findHospital(@PathVariable String hospitalId, Model model) {
-		Hospital hosSel = hospitalService.findByhospitalId(hospitalId);
-		model.addAttribute("hospitalSel", hosSel);
-		
+	@GetMapping("/hospital/myProfile")
+	public String findHospital(String hospitalId, Model model) {
 		List<DoctorVO> docList = hospitalService.getDoctorAll(hospitalId);
 		System.out.println(docList);
 		model.addAttribute("docList", docList);

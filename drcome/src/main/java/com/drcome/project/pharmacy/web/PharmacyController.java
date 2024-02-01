40392b28a746356b7f86f1510d7084c2cb5f3402
sunjuu12 <haeju7368@naver.com>
2024-02-01 23:58:46 +0900

    20240201 달력 모달

--- drcome/src/main/java/com/drcome/project/pharmacy/web/PharmacyController.java
+++ drcome/src/main/java/com/drcome/project/pharmacy/web/PharmacyController.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.drcome.project.pharmacy.PharmacyVO;
 import com.drcome.project.pharmacy.service.PharmacyService;

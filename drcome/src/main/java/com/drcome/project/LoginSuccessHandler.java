f0d1ed952e9dfe1a36da85dc0103ecb417d1f0c7
heejaeRA <rhj7369@nate.com>
2024-02-06 18:27:17 +0900

    .

--- drcome/src/main/java/com/drcome/project/LoginSuccessHandler.java
+++ drcome/src/main/java/com/drcome/project/LoginSuccessHandler.java
@@ -13,9 +13,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 
-import com.drcome.project.common.service.UserDetailVO;
-import com.drcome.project.common.service.UserMemberService;
-import com.drcome.project.common.service.UserMemberVO;
+import com.drcome.project.mem.service.UserDetailVO;
+import com.drcome.project.mem.service.UserMemberService;
+import com.drcome.project.mem.service.UserMemberVO;
 
 @Service
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {

7e6a2369b8cb8e73f05df359ceb58540c743db07
heejaeRA <rhj7369@nate.com>
2024-02-06 19:28:24 +0900

    .

--- drcome/src/main/java/com/drcome/project/FailureHandler.java
+++ drcome/src/main/java/com/drcome/project/FailureHandler.java
@@ -23,7 +23,7 @@ public class FailureHandler implements AuthenticationFailureHandler {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 
-		String message = "오류";
+		String message = "";
 
 		// 아이디 x
 		if (exception instanceof UsernameNotFoundException) {
